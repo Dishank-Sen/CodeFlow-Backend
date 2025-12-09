5540a4926398b34495b15c390a190fca4fff4ee7
Dishank-Sen <dishanksen05@gmail.com>
2025-12-09 23:33:07 +0530

    updated index.ts so it handles web socket connection

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,34 @@ import healthRoute from "./routes/healthCheckRoute.ts"
 import authRoute from "./routes/authRoute.ts"
 import layoutRoute from "./routes/layoutRoute.ts"
 import profileRoute from "./routes/profileRoute.ts"
+import http from "http"
+import { Server } from "socket.io"
 
 dotenv.config()
 const app = express()
+const server = http.createServer(app)
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173"
+];
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins
+  }
+})
+const repoPage = io.of("/repoPage")
 const PORT = Number(process.env.PORT) || 3000
-
 connectDB()
+
+repoPage.on("connection", (socket) => {
+  console.log("connected")
+
+  socket.emit("ping", ("pong"))
+
+  socket.on("disconnect", () => {
+    console.log("disconnected")
+  })
+})
 
 app.use(cookieParser());  // later add security here
 app.use(cors({
@@ -34,7 +56,7 @@ app.use("/api/v1/repo", repoRoute)
 app.use("/api/v1/profile", profileRoute)
 app.use("/api/v1/push", pushRoute)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("server running on port ",PORT)
     swaggerDocs(app, PORT)
 })
